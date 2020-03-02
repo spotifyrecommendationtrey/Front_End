f6c9d2297269c7ea22ca7b6475a21c2b5d5fd844
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-03-02 11:15:08 -0500

    added handlers for input change

--- suggestify/src/components/Register.js
+++ suggestify/src/components/Register.js
@@ -1,19 +1,29 @@
 import React, {useState, useEffect} from 'react'
 
 export default function Register(){
-    const [newUser, setNewUser] = useState('');
-    const [newPassword, setNewPassword] = useState('');
+    // Cred is short for CREDENTIALS
+    const [userCred, setUserCred] = useState({
+        username: '',
+        password: ''
+    });
+    const onInputChange = e =>{
+        setUserCred({
+            ...userCred,
+            [e.target.name]: event.target.value,
+        });
+    }
+
     return(
         <form className='user-register'>
 
         <label htmlFor='new-username'>Username: </label>
-        <input type ='text' id='new-username' name='new-username'></input>
+        <input type ='text' id='new-username' name='username' onChange={onInputChange}></input>
 
         {/* <label htmlFor='user-email'>Email: </label>
         <input type ='email' id='user-email' name='new-username'></input> */}
         {/* add minLength to password */}
         <label htmlFor='new-password'>Password: </label>
-        <input type ='text' id='new-password' name='new-password'></input>
+        <input type ='text' id='new-password' name='password' onChange={onInputChange}></input>
 
         <button type='submit'>Register</button>
     </form>
