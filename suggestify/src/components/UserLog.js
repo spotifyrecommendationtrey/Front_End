7ed8cbd7ff2f214d3ff083abe0f99b84778c1c9f
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-03-02 09:30:33 -0500

    wrapped app in router. started to build login form

--- suggestify/src/components/UserLog.js
+++ suggestify/src/components/UserLog.js
@@ -1,3 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import {Route, Link} from 'react-router-dom'
 import axios from 'axios'
+
+const UserLog = () => {
+    <div className='log-form-page'>
+        <form className='user-login'>
+            <input type='text' name ='username'></input>
+            <input type='password'></input>
+            <button type='submit'>Login</button>
+        </form>
+    </div>
+}
