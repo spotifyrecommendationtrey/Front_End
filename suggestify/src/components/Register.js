7d3bff12158b488c234328298031a2094ad563c9
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-03-03 09:24:29 -0500

    starting work

--- suggestify/src/components/Register.js
+++ suggestify/src/components/Register.js
@@ -38,30 +38,38 @@ const FormContainer = styled.div`
 const Register = ({values, touched, errors, status}) => {
     // Cred is short for CREDENTIALS
     const [userCred, setUserCred] = useState({
-        id: Date.now(),
+        id: 0,
         username: '',
         password: ''
     });
     useEffect(()=>{
-        console.log('User', status);
-        status && setUserCred(user =>[...userCred, status])
+        console.log('New User', status);
+        status && setUserCred(userCred =>[...userCred, status])
     }, [status])
 
     return(
         <FormContainer>
             <Form>
                 <label htmlFor='new-username'>Pick a Username: </label>
-                <Field type ='text' id='new-username' name='username' placeholder='Must be unique' ></Field>
+                <Field type ='text' id='new-username' name='new-username' placeholder='Must be unique' ></Field>
                 {touched.username && errors.username && <p className='error'>{errors.username}</p>}
                 {/* <label htmlFor='user-email'>Email: </label>
                 <input type ='email' id='user-email' name='new-username'></input> */}
-                {/* add minLength to password */}
                 <label htmlFor='new-password'>Choose Your Password: </label>
-                <Field type ='password' id='new-password' name='password' placeholder='Must have at least 8 characters' minLength= '8'/>
+                <Field type ='password' id='new-password' name='new-password' placeholder='Must have at least 8 characters' minLength= '8'/>
                 {touched.username && errors.username && <p className='error'>{errors.username}</p>}
-                <button className='registerSubmit' type='submit' >Register</button>
+                <button className='submitBtn' type='submit' >Register</button>
             </Form>
-            
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+            {userCred.map(user=>{
+                return(
+                    <ul key={user.id}>
+                        <li>Username: {user.username}</li>
+                        <li>Password: {user.password}</li>
+                    </ul>
+                );
+            })
+            }
         </FormContainer>
 
     )
@@ -71,7 +79,7 @@ const FormikRegister = withFormik({
 
     mapPropsToValues({id, username, password}){
         return {
-            id: id,
+            id: Date.now() || '',
             username: username || '', 
             password: password || ''
         }
@@ -79,7 +87,7 @@ const FormikRegister = withFormik({
     
     validationSchema: Yup.object().shape({
         username: Yup.string().required("Required Field"),
-        password: Yup.number().moreThan(8)
+        password: Yup.string().required("Required Field").min(8)
 
     }),
     handleSubmit(values, {resetForm, setStatus}){
@@ -93,7 +101,7 @@ const FormikRegister = withFormik({
 
         })
         .catch(err =>{
-            console.log('OOF!', err);
+            console.log('OOF!', err.response);
             resetForm();
         })
     }
