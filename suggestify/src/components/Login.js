cb179144ecfccdca7e5628e15e99894b5473a98a
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-03-03 10:54:27 -0500

    page is styled

--- suggestify/src/components/Login.js
+++ suggestify/src/components/Login.js
@@ -7,21 +7,26 @@ import * as Yup from "yup";
 
 const FormContainer = styled.div`
     margin: 0 auto;
-    border: 5px solid;
+    border: 5px outset #81b71a;
+    border-radius: 25px;
     width: 50%;
+    background-color: #141414;
+    
     Form{
         display: flex;
         flex-direction: column;
         margin: 2%;
+        border-radius: 25px;
         label{
             align-self: flex-start;
             font-size: calc(6.5px + 2vmin);
+            color: white;
         }
         input{
             margin: 2% 1%;
             width: 75%;
             min-height: 3.5vh;
-            border: 1px inset black;
+            border: 3px inset #81b71a;
             font-size: calc(4px + 2vmin);
             padding: 0 2%;
         }
@@ -30,7 +35,12 @@ const FormContainer = styled.div`
             margin-top: 3%;
             margin-bottom: 3%;
             width: 25%;
+            background-color: #81b71a;
+            border-radius: 25px;
+            border: 1px outset #141414;
+            padding: 1%;
             font-size: calc(4px + 2vmin);
+            color: white;
         }
     }
 `; 
