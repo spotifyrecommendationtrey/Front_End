571ab720f503d7ec8f4d9d2dc196caa67e518c6e
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-04 08:00:54 -0500

    pulled the components into App to clean up for render

--- suggestify/src/App.js
+++ suggestify/src/App.js
@@ -1,6 +1,38 @@
 import React from 'react';
-import UserLog from './components/UserLog'
+import {Route, Link, Switch} from 'react-router-dom'
+import styled from 'styled-components';
 import './App.css';
+
+import UserLog from './components/UserLog'
+import Login from './components/Login'
+import Register from './components/Register'
+import UserProfile from './components/UserProfile'
+
+const LogContainer = styled.div`
+    h2, h3{
+        margin: 3% 1% 2% 1%;;
+        font-size: calc(10px + 2vmin);
+        color: white;
+        text-shadow: 0 0 10px #81b71a;
+    }
+    p{
+        margin: 3% 1% 2% 1%;;
+        font-size: calc(8px + 2vmin);
+        color: white;
+        text-shadow: 0 0 10px #81b71a;
+    }
+
+    a{
+        text-decoration: none;
+        color: white;
+        text-shadow: 0 0 10px #81b71a;
+        font-size: calc(4px + 2vmin)
+    }
+    .back-link{
+        margin: 2%;
+    }
+
+`;
 
 function App() {
   return (
@@ -9,7 +41,33 @@ function App() {
         <h1>Suggestify</h1>
       </header>
       {/* We can decide where to put the login/register page. for now i'll render it here so it's easier to see/work with */}
-      <UserLog/>
+      {/* <UserLog/> */}
+        <LogContainer>
+        <Switch>
+            <Route path='/profile'>
+                <UserProfile />
+            </Route>
+            <Route path='/register'>
+                <h2>Register to Begin Exploring</h2>
+                <Register />
+                <div className='back-link'>
+                    <Link to='/'>Back</Link>
+                </div>
+            </Route>
+            <Route exact path='/'>
+                <h2>Login to Cue the Music</h2>
+                <Login />
+                <h3 className='reg-title-link'>New User?</h3>
+                <div className='register-link'>
+                    <Link to='/register'>Register Here</Link>         
+                </div>
+            </Route>
+        </Switch>
+        </LogContainer>
+    );
+
+}
+
     </div>
   );
 }
