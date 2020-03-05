35677b749271e543d87f7bfc8d0663edacf8810c
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-05 09:18:32 -0500

    mvp-recovered, I can help try and bring it home anyway I can

--- suggestify/src/utils/axiosWithAuth.js
+++ suggestify/src/utils/axiosWithAuth.js
@@ -1,11 +1,20 @@
+// import axios from 'axios';
+
+// // export default function axiosWithAuth() {
+// //   return axios.create({
+// //     headers: {
+// //       Authorization: localStorage.getItem('token')
+// //     }
+// //   });
+// // }
+
 import axios from 'axios';
-
-function axiosWithAuth() {
-  return axios.create({
-    headers: {
-      Authorization: localStorage.getItem('token')
-    }
-  });
-}
-
-export default axiosWithAuth;
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+    return axios.create({
+        baseURL: 'https://spotify3-buildweek.herokuapp.com/api',
+        headers: {
+            Authorization: token
+        }
+    });
+};
