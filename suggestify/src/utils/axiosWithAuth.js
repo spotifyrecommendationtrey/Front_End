60bcf0dc0d104cee0b8ff996507e8da57641264b
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-05 12:11:28 -0500

    should clean up the git

--- suggestify/src/utils/axiosWithAuth.js
+++ suggestify/src/utils/axiosWithAuth.js
@@ -1,20 +1,11 @@
-// import axios from 'axios';
-
-// // export default function axiosWithAuth() {
-// //   return axios.create({
-// //     headers: {
-// //       Authorization: localStorage.getItem('token')
-// //     }
-// //   });
-// // }
-
 import axios from 'axios';
-export const axiosWithAuth = () => {
-    const token = localStorage.getItem('token');
-    return axios.create({
-        baseURL: 'https://spotify3-buildweek.herokuapp.com/api',
-        headers: {
-            Authorization: token
-        }
-    });
-};
+
+function axiosWithAuth() {
+  return axios.create({
+    headers: {
+      Authorization: localStorage.getItem('token')
+    }
+  });
+}
+
+export default axiosWithAuth;
