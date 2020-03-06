da96fbdba42668473bc33bbf14eb4e22038db6c5
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-03-06 08:32:31 -0700

    Add song search and suggested

--- suggestify/src/components/Dashboard.js
+++ suggestify/src/components/Dashboard.js
@@ -2,18 +2,19 @@ import React, {useEffect} from 'react';
 import Favorites from "./Favorites";
 import Songs from "./Songs";
 import {useDispatch} from "react-redux";
-import {loadFavorites, loadSongs} from "../actions";
+import {loadFavorites} from "../actions";
+import FormikSongForm from "./SongSearch";
 
 function Dashboard() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadSongs());
     dispatch(loadFavorites());
   }, [dispatch]);
 
   return (
     <div>
+      <FormikSongForm dispatch={dispatch} />
       <Songs />
       <Favorites />
     </div>
